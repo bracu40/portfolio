6bf935c1d3b4d1f91dd5cecf07e2250cacea3373
Prabhleen Pawar <pawarp@umich.edu>
2024-06-10 02:08:56 -0400

    Fixing email form 4

--- php-scripts/send_email.php
+++ php-scripts/send_email.php
@@ -1,27 +1,17 @@
 <?php
-  // Set the recipient email address
-  $to = 'pawarp@umich.edu';
+  $to = "pawarp@umich.edu"; // this is your Email address
+  $from = $_POST['email']; // this is the sender's Email address
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $subject = "Website Contact";
+  $subject2 = "Copy of your form submission";
+  $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+  $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-  // Set the subject line
-  $subject = 'Website Contact';
-
-  // Get the form data
-  $fullname = $_POST['fullname'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-
-  // Create the email message
-  $message_body = "Full Name: $fullname\n";
-  $message_body.= "Email: $email\n";
-  $message_body.= "Message: $message";
-
-  // Set the headers
-  $headers = "From: $email\r\n";
-  $headers.= "Reply-To: $email\r\n";
-
-  // Send the email
-  mail($to, $subject, $message_body, $headers);
-
-  // Redirect to a thank you page or display a success message
-  exit;
+  $headers = "From:" . $from;
+  $headers2 = "From:" . $to;
+  mail($to,$subject,$message,$headers);
+  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+  echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+  // You can also use header('Location: thank_you.php'); to redirect
 ?>
